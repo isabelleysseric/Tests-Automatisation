5d8c3107e926611f633948e569b18c7da3b10c97
Isabelle Eysseric <32577923+isabelleysseric@users.noreply.github.com>
2025-01-15 17:25:20 -0500

    Refactoring: rename files

--- sources/program.py
+++ sources/program.py
@@ -3,7 +3,7 @@ Un script python prenant 3 arguments dont le premier est l'opération voulue et 
 les deux entiers 
 """
 
-import calc
+import calculator
 import sys
 
 print("Bienvenue dans cette petite calculatrice sous Python pour entier.\n")
@@ -13,7 +13,7 @@ def run_calc():
     op = input("Choisissez une opération entre +, -, x , / et %      : \n")
     term_1 = input("Saisissez votre premier entier \n")
     term_2 = input("Saisissez votre second entier \n")
-    res = calc.ope(op, term_1, term_2)
+    res = calculator.ope(op, term_1, term_2)
     if res != None:
         print("{} {} {} = {}".format(term_1, op, term_2, res))
         keep_going = input(
